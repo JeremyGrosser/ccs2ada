335491664efc1a58c9d632f8f0a8c68e05b4f08d
Jeremy Grosser <jeremy@synack.me>
2023-08-12 13:11:23 -0700

    deal with register groups and instances

--- templates/module.ads
+++ templates/module.ads
@@ -20,7 +20,7 @@ package {{ device.id }}.{{ module.id }} is
       --  {{ bitfield.description }}
       {% endfor %}
    end record
-      with Volatile_Full_Access, Size => {{ register.size }};
+      with Volatile, Size => {{ register.size }};
 
    for {{ register.id }}_Register use record
       {% for bitfield in register.bitfields -%}
@@ -32,18 +32,24 @@ package {{ device.id }}.{{ module.id }} is
    {% if register.description -%}
    --  {{ register.description }}
    {% endif %}{% endif %}
+
+   {% if register.instances > 1 %}
+   type {{ register.id }}_Register_Array is array (0 .. {{ register.instances - 1 }}) of {{ register.id }}_Register
+      with Component_Size => {{ register.size }};
+   {% endif %}
+
    {% endfor %}
 
    type {{ module.id }}_Peripheral is record
       {% for register in module.registers -%}
-      {{ register.id|rename }} : aliased {{ register.id }}_Register;
+      {{ register.id|rename }} : aliased {{ register.id }}_Register{% if register.instances > 1 %}_Array{% endif %};
       {% endfor %}
    end record
       with Size => {{ module.size }};
 
    for {{ module.id }}_Peripheral use record
       {% for register in module.registers -%}
-      {{ register.id|rename }} at {{ register.offset|hexformat }} range 0 .. {{ register.size-1 }};
+      {{ register.id|rename }} at {{ register.offset|hexformat }} range 0 .. {{ (register.size * register.instances) - 1 }};
       {% endfor %}
    end record;
 
